c3b13d660169593defafc7df05e6be291d2024fb
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2025-07-08 06:05:30 +0530

    my own invoice generator for service based laptop dealership company

---
+++ src/Connection.java
@@ -0,0 +1,24 @@
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sandy
+ */
+class Connection {
+
+    PreparedStatement prepareStatement(String insert_into_customers_customer_id_name_ph) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
